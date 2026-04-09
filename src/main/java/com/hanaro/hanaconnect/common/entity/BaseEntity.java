2d1cfed1d6cba313bc4e6f69848206b7001abce3
ujii <wcm9763@naver.com>
2026-04-09 22:05:40 +0900

    swagger & BaseEntity & ErrorHandler

---
+++ src/main/java/com/hanaro/hanaconnect/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.hanaro.hanaconnect.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+
+	@CreationTimestamp
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP", nullable = false)
+	private LocalDateTime updatedAt;
+
+	public String toString() {
+		return "%s - %s".formatted(createdAt, updatedAt);
+	}
+}
