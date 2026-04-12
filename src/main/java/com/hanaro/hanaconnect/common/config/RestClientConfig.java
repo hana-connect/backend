efe1a8b667ea9f15f3db3bc250ffa83b9828b1f9
seonjuuu <157812913+seonjuuu@users.noreply.github.com>
2026-04-12 14:11:46 +0900

    Configure RestClient with timeouts
    Set connection and read timeouts for RestClient.

--- src/main/java/com/hanaro/hanaconnect/common/config/RestClientConfig.java
+++ src/main/java/com/hanaro/hanaconnect/common/config/RestClientConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+
 @Configuration
 public class RestClientConfig {
 
 	@Bean
 	public RestClient.Builder restClientBuilder() {
-		return RestClient.builder();
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		requestFactory.setConnectTimeout(Duration.ofSeconds(5));
+		requestFactory.setReadTimeout(Duration.ofSeconds(30));
+
+		return RestClient.builder()
+			.requestFactory(requestFactory);
 	}
 }
