dd42ac7a5d13d3f808873f66a50efcd507c50d67
imddoy <imddoy@duksung.ac.kr>
2026-04-13 18:44:53 +0900

    REFACTOR: @PrePersist로 현재 시간 저장하도록 BaseEntity 수정 transaction createdAt을 직접 넣어주려고 변경했어요. HF-8

--- src/main/java/com/hanaro/hanaconnect/common/entity/BaseEntity.java
+++ src/main/java/com/hanaro/hanaconnect/common/entity/BaseEntity.java
@@ -2,18 +2,17 @@ package com.hanaro.hanaconnect.common.entity;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 public class BaseEntity {
 
-	@CreationTimestamp
 	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
@@ -21,11 +20,19 @@ public class BaseEntity {
 	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP", nullable = false)
 	private LocalDateTime updatedAt;
 
-	public String toString() {
-		return "%s - %s".formatted(createdAt, updatedAt);
+	@PrePersist
+	protected void onCreate() {
+		if (this.createdAt == null) {
+			this.createdAt = LocalDateTime.now();
+		}
 	}
 
-	public void setCreatedAtForInit(java.time.LocalDateTime createdAt) {
+	public void setCreatedAtForInit(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "%s - %s".formatted(createdAt, updatedAt);
 	}
 }
