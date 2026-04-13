6c230736610b9186e5cb355174f1672223812116
ujii <wcm9763@naver.com>
2026-04-13 10:40:53 +0900

    FIX: Rollback 어노테이션 삭제
    Unique 값 충돌나서 삭제했어요
    
    HF-42

--- src/test/java/com/hanaro/hanaconnect/repository/BaseRepositoryTest.java
+++ src/test/java/com/hanaro/hanaconnect/repository/BaseRepositoryTest.java
@@ -11,6 +11,5 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // H2DB를 안 쓸 때 이 어노테이션 있어야 에러 x
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Rollback(false)
 public class BaseRepositoryTest {
 }
