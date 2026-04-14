db13e3fbd8066b62efac77e0f36fdc4f42888bff
ujii <wcm9763@naver.com>
2026-04-14 20:07:42 +0900

    FIX: 로직 수정
    조회하려는 유저가 kid와 연결된 부모인지 검증하는 로직 추가
    
    HF-80

--- src/main/java/com/hanaro/hanaconnect/service/MemberServiceImpl.java
+++ src/main/java/com/hanaro/hanaconnect/service/MemberServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.hanaro.hanaconnect.common.enums.MemberRole;
 import com.hanaro.hanaconnect.dto.ConnectMemberResponseDTO;
 import com.hanaro.hanaconnect.dto.WalletResponseDTO;
 import com.hanaro.hanaconnect.entity.Member;
@@ -45,7 +46,13 @@ public class MemberServiceImpl implements MemberService{
 
 	@Override
 	public List<ConnectMemberResponseDTO> getOtherParents(Long memberId, Long kidId) {
-		boolean isRelated = relationRepository.existsByMemberIdAndConnectMemberId(memberId, kidId);
+		boolean isRelated = relationRepository
+			.existsByMember_IdAndConnectMember_IdAndConnectMemberRole(
+				kidId,          // member (아이)
+				memberId,       // connectMember (부모)
+				MemberRole.PARENT
+			);
+
 		if (!isRelated) {
 			throw new IllegalArgumentException("해당 아이와 연결된 부모만 조회할 수 있습니다.");
 		}
