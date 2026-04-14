62549dad27e641cecd583f13c119850e66a3d773
ujii <95465048+ujii@users.noreply.github.com>
2026-04-14 14:40:07 +0900

    Update TransferControllerTest.java

--- src/test/java/com/hanaro/hanaconnect/controller/TransferControllerTest.java
+++ src/test/java/com/hanaro/hanaconnect/controller/TransferControllerTest.java
@@ -24,7 +24,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hanaro.hanaconnect.common.security.TokenMemberPrincipal;
