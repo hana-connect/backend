71b3b91cc6799dc30fd694d3a924442d0e123470
ujii <95465048+ujii@users.noreply.github.com>
2026-04-14 14:42:01 +0900

    Update TransferControllerTest.java

--- src/test/java/com/hanaro/hanaconnect/controller/TransferControllerTest.java
+++ src/test/java/com/hanaro/hanaconnect/controller/TransferControllerTest.java
@@ -18,14 +18,14 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hanaro.hanaconnect.common.security.TokenMemberPrincipal;
