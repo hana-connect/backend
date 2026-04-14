7fbb913716494ddbcf9ddb62a33c3747dfc82183
seonjuuu <157812913+seonjuuu@users.noreply.github.com>
2026-04-14 15:40:38 +0900

    Add null values for account creation

--- src/main/java/com/hanaro/hanaconnect/InitLoader.java
+++ src/main/java/com/hanaro/hanaconnect/InitLoader.java
@@ -94,6 +94,7 @@ public class InitLoader implements ApplicationRunner {
 			AccountType.SAVINGS,
 			new BigDecimal("80000"),
 			kid1
+			null
 		));
 
 		accountRepository.save(createAccount(
@@ -103,6 +104,7 @@ public class InitLoader implements ApplicationRunner {
 			AccountType.SUBSCRIPTION,
 			new BigDecimal("120000"),
 			kid1
+			null
 		));
 
 
@@ -155,6 +157,7 @@ public class InitLoader implements ApplicationRunner {
 			AccountType.SUBSCRIPTION,
 			new BigDecimal("150000"),
 			parent1
+			null
 		));
 
 		accountRepository.save(createAccount(
@@ -164,6 +167,7 @@ public class InitLoader implements ApplicationRunner {
 			AccountType.PENSION,
 			new BigDecimal("300000"),
 			parent1
+			null
 		));
 		linkedAccountRepository.save(
 			LinkedAccount.builder()
