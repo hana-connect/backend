34df3c44e4812963a05355fdcc5ed853fd6a99f8
seonjuuu <157812913+seonjuuu@users.noreply.github.com>
2026-04-14 18:12:16 +0900

    Update SavingsTransferRequestDTO.java

--- src/main/java/com/hanaro/hanaconnect/dto/SavingsTransferRequestDTO.java
+++ src/main/java/com/hanaro/hanaconnect/dto/SavingsTransferRequestDTO.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +25,7 @@ public class SavingsTransferRequestDTO {
 	private BigDecimal amount;
 
 	@NotBlank(message = "간편비밀번호는 필수입니다.")
+	@Size(min = 6, max = 6, message = "간편 비밀번호는 6자리여야 합니다.")
 	private String password;
 
 	private String content;  // 적금 편지
