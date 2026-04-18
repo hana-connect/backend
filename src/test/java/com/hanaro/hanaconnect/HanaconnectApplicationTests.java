a569b0142df94274ea2d59ddbbf1015826f9d8ab
seonjuuu <157812913+seonjuuu@users.noreply.github.com>
2026-04-19 01:55:38 +0900

    Add ActiveProfiles annotation for local profile

--- src/test/java/com/hanaro/hanaconnect/HanaconnectApplicationTests.java
+++ src/test/java/com/hanaro/hanaconnect/HanaconnectApplicationTests.java
@@ -2,8 +2,10 @@ package com.hanaro.hanaconnect;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class HanaconnectApplicationTests {
 
 	@Test
